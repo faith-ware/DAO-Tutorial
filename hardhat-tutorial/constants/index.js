d83daacc98baeaf64be8144842ca796a9b84c25b
faith-ware <faithade69@gmail.com>
2022-04-22 16:31:51 -0500

    First commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x2079B609b3a746e8913f5E14B19feF2F2114e554";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
